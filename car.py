7032b5898de352b6f0f7f0d4ae54ba8653ecd82e
David Cleary <davidcgcleary@gmail.com>
2023-04-28 11:57:58 +0000

    Draw track and working on curving track.

--- car.py
+++ car.py
@@ -8,6 +8,7 @@ Author:    David Cleary
 Licencing: Copyright 2023 SuniTAFE. All rights reserved.
 """
 
+import random
 import curses
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
@@ -15,6 +16,8 @@ from curses.textpad import Textbox, rectangle
 BLUEWHITE = 1
 BLACKWHITE = 2
 REDWHITE = 3
+
+DRAWTIMEOUT = 10000
 
 def getDims(win, pause):
     """ 
@@ -57,27 +60,65 @@ def drawCar(win, row, col, moveKey = None):
         win.addstr(row, col, chr(32))
         win.addstr(row + 1, col - 1, chr(32))
 
+def createTrack(rows, cols):
+    left = int((cols / 2) - 10)
+    track = []
+    for r in range(rows):
+        track.append(left)
+    return track
+
+curveLeft = False 
+curveCount = 0
+
+def drawTrack(win, track):
+    global curveLeft
+    global curveCount
+    if curveLeft:
+        newCol = track[0] - 1
+        track.insert(0, newCol)
+        track.pop(-1)
+        curveCount -= 1
+        curveLeft = False if curveCount == 0 else True
+    else:
+        curveCount = int(random.random() * 100)
+        if curveCount < 10:
+            curveLeft = True
+        
+    win.erase()
+    row = 0
+    for col in track:
+        win.addstr(row, col,"|                    |")
+        row += 1 
+        
 def cursesMain(stdScr):
     stdScr = curses.initscr()
     curses.init_pair(BLUEWHITE, curses.COLOR_BLUE, curses.COLOR_WHITE)
     curses.init_pair(BLACKWHITE, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(REDWHITE, curses.COLOR_RED, curses.COLOR_WHITE)
     stdScr.bkgd(' ', curses.color_pair(BLUEWHITE))
-    rows, cols = getDims(stdScr, True)
+    rows, cols = getDims(stdScr, False)
+    stdScr.nodelay(True)
     stdScr.clear()
 
+    track = createTrack(rows, cols)
+    
     carR = rows - 2
     carC = int((cols - 2) / 2)
     key = None
+    drawTimer = DRAWTIMEOUT
     while True:
+        if drawTimer < 0: 
+            drawTrack(stdScr, track)
+            drawTimer = DRAWTIMEOUT
         drawCar(stdScr, carR, carC, key)
         hideCursor(stdScr, rows, cols)
         stdScr.refresh()
         key = stdScr.getch()
         if key == curses.KEY_LEFT:
-            carC -= 1
+            carC = carC - 1 if carC > 0 else carC
         elif key == curses.KEY_RIGHT:
-            carC += 1
+            carC = carC + 1 if carC < cols - 6 else carC
+        drawTimer -= 1
             
     stdScr.addstr(2, 0, "Press any key to continue.")
     stdScr.getch()
